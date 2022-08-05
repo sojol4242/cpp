49086138b3852250c55d9c57bcba2518e8daf05a
Md Moniruzzaman <bsmrstueee90@gmail.com>
2022-08-05 11:48:17 +0600

    Start Cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  
+using namespace std;  
+int main () {  
+   string name;
+   cout << "Enter a String: ";
+   getline (cin, name);   
+   cout << "Result : " << name << endl;   
+   return 0;  
+} 
